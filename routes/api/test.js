0b59c8a386bfa042d861447582e1fc771614e2db
VizioJoash <113297969+VizioJoash@users.noreply.github.com>
2023-04-27 01:32:51 +0800

    Update test.js

--- routes/api/test.js
+++ routes/api/test.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-router.get('/',
+router.get('/', (req, res) => {
     console.log("test");
-);
+});
 module.exports = router;
