66b9eb7f9b4eb09e25e7d0d1b382d070d33b1fb2
VizioJoash <113297969+VizioJoash@users.noreply.github.com>
2023-06-07 03:10:06 +0800

    s

--- src/api/routes/teams/teams.ts
+++ src/api/routes/teams/teams.ts
@@ -1,5 +1,5 @@
 import Router from "@koa/router";
-import { IsEligibleToPlay } from "../../../middleware"
+import { IsEligibleToPlay, isLoggedIn } from "../../../middleware"
 import { ParameterizedContext } from "koa";
 import { Invite, Team } from "../../../Models/team";
 import { OsuUser } from "../../../Models/user";
@@ -10,7 +10,14 @@ teamsRouter.get("/", async (ctx: ParameterizedContext<any>, next) => {
     const teams = await Team.find({
         relations: ['player1', 'player2']
     });
-    ctx.body = teams;
+
+    const promises = teams.map(async team => {
+        return team.getInfo();
+    });
+    
+    const teamInfoArray = await Promise.all(promises);
+    console.log(teamInfoArray);
+    ctx.body = teamInfoArray;
 });
 
 teamsRouter.get("/invites", async (ctx: ParameterizedContext<any>, next) => {
@@ -40,15 +47,30 @@ teamsRouter.get("/invites", async (ctx: ParameterizedContext<any>, next) => {
     ctx.body = inviteInfoArray;
 });
 
-teamsRouter.post("/invites/send", async (ctx: ParameterizedContext<any>, next) => {
-    
-    if (!ctx.isAuthenticated || !ctx.session.userID) {
-        ctx.body = { error: "Your session has expired, please log in again." }; 
+teamsRouter.post("/invites/send", isLoggedIn, async (ctx: ParameterizedContext<any>, next) => {
+
+    if (!ctx.request.body["invitee"] || ctx.request.body["invitee"] === "") {
+        ctx.body = { error: "User does not exist." }; 
         return;
     }
 
-    if (!ctx.request.body["invitee"] || ctx.request.body["invitee"] === "" || ctx.request.body["invitee"] == ctx.session.userID) {
-        ctx.body = { error: "You cannot invite this player." }; 
+    const [sender, invitee] = await Promise.all([
+        OsuUser.findOne({where: { userID: ctx.session.userID }, relations: ["discord"]}),
+        OsuUser.findOne({where: { userID: ctx.request.body["invitee"] }, relations: ["discord"]})
+    ]);
+
+    if (!sender) {
+        ctx.body = { error: "Invalid sender." };
+        return;
+    }
+
+    if (!invitee) {
+        ctx.body = { error: "User does not exist." };
+        return;
+    }
+
+    if (invitee.is_restricted || (await invitee.getInfo()).staff.headStaff || invitee.userID == ctx.session.userID) {
+        ctx.body = { error: "You cannot invite this player." };
         return;
     }
 
@@ -63,18 +85,6 @@ teamsRouter.post("/invites/send", async (ctx: ParameterizedContext<any>, next) =
 
     if (invite) {
         ctx.body = { error: 'You have already invited this player.' };
-        return;
-    }
-
-    const [sender, invitee] = await Promise.all([
-        OsuUser.findOne({where: { userID: ctx.session.userID }}),
-        OsuUser.findOne({where: { userID: ctx.request.body["invitee"] }})
-    ]);
-
-    console.log(sender);
-
-    if (!sender || !invitee) {
-        ctx.body = { error: "Invalid sender or invitee." };
         return;
     }
 
@@ -111,7 +121,7 @@ teamsRouter.post("/invites/accept", async (ctx: ParameterizedContext<any>, next)
                 { userID: ctx.request.body["invite"]["sender"]},
                 { userID: ctx.session.userID }
             ]
-        })
+        });
 
         const team = new Team;
         team.team_name = "Team " + Math.floor(Math.random() * 9000 + 1000);
@@ -127,6 +137,32 @@ teamsRouter.post("/invites/accept", async (ctx: ParameterizedContext<any>, next)
         ctx.body = { "error": "Error while accepting invite: " + err };
         console.log(err);
     }
+});
+
+
+teamsRouter.post("/invites/decline", isLoggedIn, async (ctx: ParameterizedContext<any>, next) => { 
+    try {
+        const invite = await Invite.findOne({
+            where: {
+                sender: { userID: ctx.request.body["invite"]["sender"] },
+                invitee: { userID:  ctx.session.userID }
+            },
+            relations: [ "sender", "invitee" ]
+        })
+
+        if (!invite) {
+            ctx.body = { "error": "Invalid invite" };
+            return;
+        }
+        
+        await invite.remove();
+        ctx.body = { "success": `You have declined an invite from '${invite.sender.username}'.`}
+
+    } catch (err) {
+        ctx.body = { "error": "Error while declining invite: " + err };
+        console.log(err);
+    }
+
 });
 
 export default teamsRouter;
--- src/api/routes/users/user.ts
+++ src/api/routes/users/user.ts
@@ -2,7 +2,7 @@ import Router from "@koa/router";
 import { OsuUser } from "../../../Models/user";
 import { isLoggedIn } from "../../../middleware";
 import { appDataSource } from "../../../index";
-import { ILike, Like } from "typeorm";
+import { ILike, Like, Raw } from "typeorm";
 import { ParameterizedContext } from "koa";
 import { userInfo } from "os";
 
@@ -16,7 +16,10 @@ userRouter.get("/me", isLoggedIn, async (ctx: ParameterizedContext<any>, next) =
         relations: ['discord']
     });
 
-    if (!user) return;
+    if (!user) {
+        ctx.logout();
+        return;
+    };
 
     console.log(user);
     ctx.body = await user.getInfo();
@@ -44,6 +47,34 @@ userRouter.get("/search", async (ctx: ParameterizedContext<any>, next) => {
     
     const userInfoArray = await Promise.all(promises);
     ctx.body = userInfoArray;
+});
+
+userRouter.get("/search-exact", async (ctx: ParameterizedContext<any>, next) => {
+
+    if (!ctx.request.query["playerSearchQuery"]) {
+        ctx.body = [];
+        return;
+    }
+
+    const searchQuery = ctx.request.query["playerSearchQuery"].toString().toLowerCase();
+
+    const user = await OsuUser.findOne({
+        where: [
+          { userID: Number.isNaN(Number(ctx.request.query["playerSearchQuery"])) ? -1 : Number(ctx.request.query["playerSearchQuery"]) },
+          { username: Raw(alias => `${alias} ILIKE '%${searchQuery}%'`) },
+        ],
+        relations: ['discord']
+    });
+    
+    if (!user) {
+        ctx.body = { 'error': "no user found" };
+        return;
+    }
+    
+    const info = await user.getInfo();
+
+    console.log(info);
+    ctx.body = [info];
 });
 
 userRouter.get("/all", async (ctx: ParameterizedContext<any>, next) => {
