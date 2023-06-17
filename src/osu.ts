2b951efe87ce254c437958caaf2dcfc1ebac2431
VizioJoash <113297969+VizioJoash@users.noreply.github.com>
2023-06-18 02:17:48 +0800

    fixed impoort error

--- src/osu.ts
+++ src/osu.ts
@@ -1,5 +1,5 @@
 import { config } from "node-config-ts";
-import { osuAPIV2 } from "../src/Interfaces/osuAPIV2";
+import { osuAPIV2 } from "./Interfaces/osuAPIV2";
 
 // API v2
 const osuV2Client = new osuAPIV2(config.osu.v2.clientId, config.osu.v2.clientSecret);
